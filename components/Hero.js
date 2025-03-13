44e98d0e1e647ea279ba5eb2cfa89148089de2cc
Toshe00 <rpsbatllepro@gmail.com>
2025-03-13 16:06:15 +0100

    Add files via upload

--- components/Hero.js
+++ components/Hero.js
@@ -5,58 +5,61 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen bg-[#9DCAFE] flex flex-col justify-center items-center overflow-hidden">
       {/* Hands */}
-      <div className="absolute left-0 top-4 bottom-10 -ml-4 2xl:-ml-24 2xl:-top-10">
+      <div className="absolute left-0 top-0 md:top-1/2 transform md:-translate-y-1/2">
         <Image
-          src="/images/left-hand.png"
+          src="/images/left-hand2.png"
           alt="Left Hand"
-          width={600}
-          height={600}
-          className="w-[300px] h-[300px] object-center 2xl:w-[1400px] 2xl:h-[1400px] object-contain"
+          width={1192}
+          height={1080}
+          className="w-[400px] h-[400px] md:w-[600px] md:h-[600px] xl:w-[1000px] xl:h-[1000px] object-cover"
         />
       </div>
 
-      <div className="absolute right-0 top-7 bottom-1 -mr-10 2xl:top-2">
+      <div className="absolute right-0 top-0 md:top-1/2 transform md:-translate-y-1/2">
         <Image
-          src="/images/right-hand.png"
+          src="/images/right-hand2.png"
           alt="Right Hand"
-          width={600}
-          height={600}
-          className="w-[300px] h-[300px] object-center 2xl:w-[1400px] 2xl:h-[1400px] 2xl:object-contain"
+          width={940}
+          height={998}
+          className="w-[400px] h-[400px] md:w-[600px] md:h-[600px] xl:w-[850px] xl:h-[850px] object-cover"
         />
-        
       </div>
-
 
       {/* Play Button */}
-      <a href="#" className="bg-white text-black text-3xl font-bold italic px-16 py-12 border-l-4 border-b-4 border-pink-500 shadow-[6px_6px_0px_0px_black] transition-all duration-300 hover:bg-pink-400 z-10">
+      <a
+        href="#"
+        className="bg-white text-black text-xl md:text-3xl font-bold italic px-12 py-6 md:px-16 md:py-8 border-l-4 border-b-4 border-pink-500 shadow-[4px_4px_0px_0px_black] transition-all duration-300 hover:bg-pink-400 z-10 mt-12"
+      >
         PLAY
       </a>
 
       {/* RPS Battle Text */}
-      <h1 className="mt-6 text-5xl font-bold italic text-white">RPS battle</h1>
+      <h1 className="mt-4 text-3xl md:text-5xl font-bold italic text-white drop-shadow-md">
+        RPS battle
+      </h1>
 
       {/* Bottom Hands */}
-      <div className="absolute bottom-20 flex gap-10">
+      <div className="absolute bottom-8 md:bottom-16 flex gap-4 md:gap-8">
         <Image
           src="/images/hand1.png"
           alt="Hand 1"
-          width={120} // Increased size
-          height={120}
-          className="w-auto h-28 2xl:w-[150px] 2xl:h-[150px]"
+          width={200}
+          height={200}
+          className="w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 object-contain hover:scale-110 transition-all duration-300"
         />
         <Image
           src="/images/hand2.png"
           alt="Hand 2"
-          width={120} // Increased size
-          height={120}
-          className="w-auto h-28 2xl:w-[150px] 2xl:h-[150px]"
+          width={200}
+          height={200}
+          className="w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 object-contain hover:scale-110 transition-all duration-300"
         />
         <Image
           src="/images/hand3.png"
           alt="Hand 3"
-          width={120} // Increased size
-          height={120}
-          className="w-auto h-28 2xl:w-[150px] 2xl:h-[150px]"
+          width={200}
+          height={200}
+          className="w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 object-contain hover:scale-110 transition-all duration-300"
         />
       </div>
     </section>
