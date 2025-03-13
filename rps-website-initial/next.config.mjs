7d75f258248a5090356774099a3152f9ab2c3da2
Toshe00 <rpsbatllepro@gmail.com>
2025-03-13 14:04:04 +0100

    Delete rps-website-initial directory

--- rps-website-initial/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ["picsum.photos"], // Allow Lorem Picsum images
-      },
-      devIndicators: false
-};
-
-export default nextConfig;
